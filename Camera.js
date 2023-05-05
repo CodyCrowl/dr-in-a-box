16e8306c5815a02ea9878285a2e13cb15470caa5
David <david555cano@gmail.com>
2023-05-04 22:20:34 -0700

    FACE

--- Camera.js
+++ Camera.js
@@ -1,65 +1,82 @@
-import { Permissions, Camera, FaceDetector, } from "expo";
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { Camera } from 'expo-camera';
+import * as FaceDetector from 'expo-face-detector';
 
-async function componentWillMount() 
-    const { status } =await Permissions.askAsync(Permissions.CAMERA);
-    this.setState({hasCameraPermission:status==='granted'});
+export default function App() {
+  const [hasPermission, setHasPermission] = React.useState();
+  const [faceData, setFaceData] = React.useState([]);
 
-    return (
-      <View style={styles.container}>
-      <Camera
-    style={styles.camera}
-    type={'front'}
-    ref={ref => { this.camera = ref; }}
-    onFacesDetected={this.handleFacesDetected}
-    faceDetectorSettings={{
-        mode: FaceDetector.Constants.Mode.fast,
-        detectLandmarks: FaceDetector.Constants.Mode.none,
-        runClassifications: FaceDetector.Constants.Mode.none,
-}}>
-<TouchableOpacity
-    style={{
-        flex: 1,
-        flexDirection: 'row',
-        alignSelf: 'flex-end',
-        alignItems: 'flex-end',
-    }}
-    onPress={() => this.snap(false)}>
-    <Text
-        style={{ fontSize: 18, marginBottom: 10, color: 'white' }}>
-        {' '}Enroll{' '}
-    </Text>
-</TouchableOpacity>
-<TouchableOpacity
-    style={{
-        flex: 1,
-        flexDirection: 'row',
-        alignSelf: 'flex-end',
-        alignItems: 'flex-end',
-    }}
-    onPress={() => this.snap(true)}>
-    <Text
-        style={{ fontSize: 18, marginBottom: 10, color: 'white' }}>
-        {' '}Recognize{' '}
-    </Text>
-</TouchableOpacity>
-</Camera>
-</View>
-);
+  React.useEffect(() => {
+    (async () => {
+      const {status} = await Camera.requestCameraPermissionsAsync();
+      setHasPermission(status === "granted");
+    })();
+  }, []);
 
-snap = async (recognize) => {
-  try {
-      if (this.camera) {
-          let photo = await this.camera.takePictureAsync({ base64: true });
-          if(!faceDetected) {
-              alert('No face detected!');
-              return;
-          }
-
-          const userId = makeId();
-          const { base64 } = photo;
-          this[recognize ? 'recognize' : 'enroll']({ userId, base64 });
-      }
-  } catch (e) {
-      console.log('error on snap: ', e)
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
   }
-};
+
+  function getFaceDataView() {
+    if (faceData.length === 0) {
+      return (
+        <View style={styles.faces}>
+          <Text style={styles.faceDesc}>No faces :(</Text>
+        </View>
+      );
+    } else {
+      return faceData.map((face, index) => {
+        const eyesShut = face.rightEyeOpenProbability < 0.4 && face.leftEyeOpenProbability < 0.4;
+        const winking = !eyesShut && (face.rightEyeOpenProbability < 0.4 || face.leftEyeOpenProbability < 0.4);
+        const smiling = face.smilingProbability > 0.7;
+        return (
+          <View style={styles.faces} key={index}>
+            <Text style={styles.faceDesc}>Eyes Shut: {eyesShut.toString()}</Text>
+            <Text style={styles.faceDesc}>Winking: {winking.toString()}</Text>
+            <Text style={styles.faceDesc}>Smiling: {smiling.toString()}</Text>
+          </View>
+        );
+      });
+    }
+  }
+
+  const handleFacesDetected = ({ faces }) => {
+    setFaceData(faces);
+    console.log(faces);
+  }
+
+  return (
+    <Camera 
+      type={Camera.Constants.Type.front}
+      style={styles.camera}
+      onFacesDetected={handleFacesDetected}
+      faceDetectorSettings={{
+        mode: FaceDetector.FaceDetectorMode.fast,
+        detectLandmarks: FaceDetector.FaceDetectorLandmarks.none,
+        runClassifications: FaceDetector.FaceDetectorClassifications.none,
+        minDetectionInterval: 100,
+        tracking: true
+      }}>
+      {getFaceDataView()}
+    </Camera>
+  );
+}
+
+const styles = StyleSheet.create({
+  camera: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  faces: {
+    backgroundColor: '#ffffff',
+    alignSelf: 'stretch',
+    alignItems: 'center',
+    justifyContent: 'center',
+    margin: 16
+  },
+  faceDesc: {
+    fontSize: 20
+  }
+});
