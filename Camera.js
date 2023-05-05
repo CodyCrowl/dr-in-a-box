55a47e766821936abff0ee874654dcca0665a1c5
David <david555cano@gmail.com>
2023-05-04 21:36:10 -0700

    Facial

--- Camera.js
+++ Camera.js
@@ -1,57 +1,65 @@
-import FaceDetection, { FaceDetectorContourMode, FaceDetectorLandmarkMode, FaceContourType } from "react-native-face-detection";
+import { Permissions, Camera, FaceDetector, } from "expo";
 
-function FacialRecognition() {
-  const devices = useCameraDevices('front-camera');
-  const device = devices.back;
-  const isAppForeground = useIsAppForeground();
+async function componentWillMount() 
+    const { status } =await Permissions.askAsync(Permissions.CAMERA);
+    this.setState({hasCameraPermission:status==='granted'});
 
-  if (device == null) return <LoadingView />
-  return (
-    <Camera
-      style={StyleSheet.absoluteFill}
-      device={device}
-      isActive={useIsAppForeground}
-    />
-  )
-}
-<Camera {...props} photo={true} />
-const photo = await camera.current.takePhoto({
-  flash: 'on'
-})
+    return (
+      <View style={styles.container}>
+      <Camera
+    style={styles.camera}
+    type={'front'}
+    ref={ref => { this.camera = ref; }}
+    onFacesDetected={this.handleFacesDetected}
+    faceDetectorSettings={{
+        mode: FaceDetector.Constants.Mode.fast,
+        detectLandmarks: FaceDetector.Constants.Mode.none,
+        runClassifications: FaceDetector.Constants.Mode.none,
+}}>
+<TouchableOpacity
+    style={{
+        flex: 1,
+        flexDirection: 'row',
+        alignSelf: 'flex-end',
+        alignItems: 'flex-end',
+    }}
+    onPress={() => this.snap(false)}>
+    <Text
+        style={{ fontSize: 18, marginBottom: 10, color: 'white' }}>
+        {' '}Enroll{' '}
+    </Text>
+</TouchableOpacity>
+<TouchableOpacity
+    style={{
+        flex: 1,
+        flexDirection: 'row',
+        alignSelf: 'flex-end',
+        alignItems: 'flex-end',
+    }}
+    onPress={() => this.snap(true)}>
+    <Text
+        style={{ fontSize: 18, marginBottom: 10, color: 'white' }}>
+        {' '}Recognize{' '}
+    </Text>
+</TouchableOpacity>
+</Camera>
+</View>
+);
 
-async function processFaces(imagePath) {
-  const options = {
-    landmarkMode: FaceDetectorLandmarkMode.ALL,
-    contourMode: FaceDetectorContourMode.ALL
-  };
+snap = async (recognize) => {
+  try {
+      if (this.camera) {
+          let photo = await this.camera.takePictureAsync({ base64: true });
+          if(!faceDetected) {
+              alert('No face detected!');
+              return;
+          }
 
-  const faces = await FaceDetection.processImage(imagePath, options);
-
-  faces.forEach(face => {
-    console.log('Head rotation on X axis: ', face.headEulerAngleX);
-    console.log('Head rotation on Y axis: ', face.headEulerAngleY);
-    console.log('Head rotation on Z axis: ', face.headEulerAngleZ);
-
-    console.log('Left eye open probability: ', face.leftEyeOpenProbability);
-    console.log('Right eye open probability: ', face.rightEyeOpenProbability);
-    console.log('Smiling probability: ', face.smilingProbability);
-
-    face.faceContours.forEach(contour => {
-      if (contour.type === FaceContourType.FACE) {
-        console.log('Face outline points: ', contour.points);
+          const userId = makeId();
+          const { base64 } = photo;
+          this[recognize ? 'recognize' : 'enroll']({ userId, base64 });
       }
-    });
-
-    face.landmarks.forEach(landmark => {
-      if (landmark.type === FaceLandmarkType.LEFT_EYE) {
-        console.log('Left eye outline points: ', landmark.points);
-      } else if (landmark.type === FaceLandmarkType.RIGHT_EYE) {
-        console.log('Right eye outline points: ', landmark.points);
-      }
-    });
-  });
-}
-
-const imagePath = '/path/to/face-image.png';
-
-processFaces(imagePath).then(() => console.log('Finished processing file.'));
+  } catch (e) {
+      console.log('error on snap: ', e)
+  }
+};
